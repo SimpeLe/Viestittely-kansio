89580ad98228ec36429f0234869ec63b08e3e7da
SimpeLe <tago@meiliboxi.fi>
2023-07-03 09:55:20 +0300

    ohjeisiin päivitä pyqt5

--- Simo/ui_kotisivu.py
+++ Simo/ui_kotisivu.py
@@ -1,11 +1,13 @@
+# 20230703 9:55
 # Simo-hakemistossa
 # - python -m venv env
 # - env\scripts\activate
 # - pip list
 # - pip install pyqt5 pyqt5-tools
+# - python.exe -m pip install --upgrade pip
 # (desingner kansio on syvällä kansiorakenteessa, seuraavassa oikotie)
 # - qt5-tools designer
-# (kopio pyqt\env\scripts\pyuic5.exe työkansioon)
+# - kopio pyqt\env\scripts\pyuic5.exe työkansioon
 # - pyuic5 -x -o ui_login_application.py ui_login_application.ui (kansiossa jossa pyuic5.e3xe on)
 # (seuraava käynnistää ohjelman. Katso että komentokehotteen edessä lukee "(env)")
 # (jos ei lue "(env)", käynnistä virtuaaliympäristö env\scripts\activate)
