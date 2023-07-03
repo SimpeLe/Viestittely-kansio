27112a220bdd0a22211ee704cf6f839f32bdd3e6
SimpeLe <tago@meiliboxi.fi>
2023-07-03 16:59:00 +0300

    main.py:hyn pari riviä

--- Simo/main.py
+++ Simo/main.py
@@ -10,6 +10,8 @@
 # - qt5-tools designer
 # - resurssienhallinnassa kopio pyqt\env\scripts\pyuic5.exe työkansioon
 # - pyuic5 -x -o ui_kotisivu.py ui_kotisivu.ui (kansiossa jossa pyuic5.e3xe on)
+# - pyuic5 -x -o ui_laheta.py ui_laheta.ui
+# - pyuic5 -x -o ui_vastaanota.py ui_vastaanota.ui
 # (seuraava käynnistää ohjelman. Katso että komentokehotteen edessä lukee "(env)")
 # (jos ei lue "(env)", käynnistä virtuaaliympäristö env\scripts\activate)
 # - python ui_kotisivu.py 
@@ -17,6 +19,8 @@
 # - tee törkeesti muutoksia main.py fileen Code Studiossa
 from ui_kotisivu import Ui_MainWindow
 from ui_laheta import Ui_MainWindow
+from ui_vastaanota import Ui_MainWindow
+
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
