2f62880e0db599c5a11a282e3546b4c28f0cc3e3
SimpeLe <tago@meiliboxi.fi>
2023-08-11 12:38:55 +0300

    viestitiedostot

--- Simo/socketReceiveFile.py
+++ Simo/socketReceiveFile.py
@@ -10,7 +10,7 @@ import time
 from PyQt5 import QtWidgets as qtw
 
 
-def RecvFileViaIP(port = 5001, sekuntia = 5):
+def RecvFileViaIP(port = 5001, sekuntia = 15):
     # device's IP address
     SERVER_HOST = "0.0.0.0"
     SERVER_PORT = port
