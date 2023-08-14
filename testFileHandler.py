f791d844fc3a099b6574567bd556c22a0230db86
vilvuor <ville.vuori@welho.com>
2023-08-14 22:16:23 +0300

    Creted new  test cases

--- testFileHandler.py
+++ testFileHandler.py
@@ -6,18 +6,23 @@ import os.path
 
 class TestHandler(unittest.TestCase):
 
-
-    def test_createOneSourceCharacterList(self):
+    def test_createOneSourceCharacterListLength(self):
         expected = 103
         res = fileMessageHandler.createOneSourceCharacterList()
         result = len(res)
         self.assertEqual(result, expected)
 
-    def test_createOneSourceCharacterListCharacter(self):
+    def test_createOneSourceCharacterListLegalCharacter(self):
         expected = True
         res = fileMessageHandler.createOneSourceCharacterList()
         result = "a" in res
         self.assertEqual(result, expected)
+
+    def test_createOneSourceCharacterListIllegalCharacter(self):
+        expected = True
+        res = fileMessageHandler.createOneSourceCharacterList()
+        result = "û" in res
+        self.assertFalse(result, expected)
 
     def test_createSourceCharacterFile(self):
         expected = True
@@ -37,14 +42,28 @@ class TestHandler(unittest.TestCase):
         result = os.path.isfile('messageKeyFile.txt')
         self.assertEqual(result, expected)
 
-    def test_loadMessageListFromFile(self):
+    def test_loadMessageListFromFileLength(self):
         expected = 1_000_000
         list = []
         list = fileMessageHandler.loadMessageListFromFile()
         result = len(list)
         self.assertEqual(result, expected)
 
-    
+    def test_loadLocationListFromFileLength(self):
+        expected = 10_000
+        list = []
+        list = fileMessageHandler.loadLocationListFromFile()
+        result = len(list)
+        self.assertEqual(result, expected)
+
+    def test_searchIndexFromCharFile(self):
+        expected = fileMessageHandler.searchIndexFromCharFile()
+
+        list = fileMessageHandler.searchIndexFromCharFile()
+        self.assertListEqual(list, expected)
+        
+    def test_findIndexByLocationFromMessage():
+        fileReceiverHandler.findIndexByLocationFromMessage()
 
 if __name__ == "__main__":
 
